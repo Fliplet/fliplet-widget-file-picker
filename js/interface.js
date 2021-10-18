ebc30acae87562976c26fb562aa9675b66219420
Hugo Carneiro <hello@thegeekdeveloper.com>
2021-10-18 18:47:47 +0100

    Moved to fliplet-studio-ui

--- js/interface.js
+++ js/interface.js
@@ -436,20 +436,6 @@ function displayDeletionConfirmation(type, id) {
   });
 }
 
-function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) {
-    return '0 Bytes';
-  }
-
-  var k = 1024;
-  var dm = decimals < 0 ? 0 : decimals;
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-  var i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
-
 function toggleStorageUsage(appId) {
   // Show or hide the storage usage UI
   $('.storage-holder')[appId ? 'removeClass' : 'addClass']('hidden');
